621554ecad816b9cd1327b7c73694ae29934629b
gherrera <gerardo.herrera@patagoniansys.com>
2019-07-26 15:30:49 -0300

    Funcionando

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,16 @@
 import { Component } from '@angular/core';
-
+import html2pdf from 'html2pdf.js';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'submit-html';
+  constructor(){
+
+  }
+  onSubmit(f){
+    console.log(f.value.convertir);
+    html2pdf(f.value.convertir);
+  }
 }
