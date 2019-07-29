ea13a9209954bbf88322f1189101b7e1f4117b90
gherrera <gerardo.herrera@patagoniansys.com>
2019-07-29 16:24:52 -0300

    fix

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,4 @@
 import { Component } from '@angular/core';
-import { DomSanitizer } from '@angular/platform-browser';
 import html2pdf from 'html2pdf.js';
 @Component({
   selector: 'app-root',
@@ -7,19 +6,15 @@ import html2pdf from 'html2pdf.js';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  constructor(private sanitizer: DomSanitizer){
-
-  }
+  constructor(){  }
   onSubmit(f){
-    console.log(html2pdf());
     html2pdf(
-      this.sanitizer.sanitize(1, f.value.convertir),
+      f.value.convertir,
       {//opt
         pagebreak: {
           mode:['css'],
           before:['.pagebreak']
         }
       });
-    // html2pdf.Worker
   }
 }
